90bfbb3d97ebfb392a9c466f3573179e85b0ee4e
JoseManuel-Feliz <jmgarcia1718@gmail.com>
2024-08-30 03:09:35 +0200

    Snack 1 added createPlacecard function

--- JS/script.js
+++ JS/script.js
@@ -19,3 +19,40 @@ Generiamo e stampiamo in console la lista per i segnaposto.
 let guests = ['Brad Pitt', 'Johnny Depp', 'Lady Gaga', 'Cristiano Ronaldo', 'Georgina Rodriguez', 'Chiara Ferragni', 'George Clooney', 'Amal Clooney', 'Fedez', 'Amadeus', 'Fiorello']
 console.log(guests)
 
+//Funzione per creare il segnaposto
+const createPlaceCard = (guest, i) => guestsList.push({ tableName: 'Tavolo Vip', guetsName: guest, place: i + 1 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* 
+*SNACK 2*
+Abbiamo un elenco degli studenti di una facoltà, identificati da id, Nome e somma totale dei loro voti di esame...
+1. Per preparare l'aula di un nuovo corso, dobbiamo stampare le targhe col nome degli studenti: creare una lista contenente il loro nome tutto in maiuscolo
+ES (Marco della Rovere => MARCO DELLA ROVERE);
+2. Dobbiamo creare una lista di tutti gli studenti che hanno un totale di voti superiore a 70
+3. Dobbiamo creare una lista di tutti gli studenti che hanno un totale di voti superiore a 70 e id superiore a 120
+Questo è l'elenco degli studenti:
+Id  Name                Grades
+213 Marco della Rovere      78
+110 Paola Cortellessa       96
+250 Andrea Mantegna 	    48
+145 Gaia Borromini          74
+196 Luigi Grimaldello 	    68
+102 Piero della Francesca   50
+120 Francesca da Polenta    84
+ */
